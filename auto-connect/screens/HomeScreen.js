6839ec2cd22563298e90e2aea69c7eb97d080ae2
sonu12485 <sonusaikishan@gmail.com>
2019-02-10 02:31:47 +0530

    Added header

--- auto-connect/screens/HomeScreen.js
+++ auto-connect/screens/HomeScreen.js
@@ -4,6 +4,9 @@ import { View, Text, StyleSheet } from "react-native";
 import { fetchUserDetails } from "../actions/userDetails";
 
 import { connect } from "react-redux";
+
+import { Ionicons } from '@expo/vector-icons';
+
 import Map from "../components/Map";
 
 class HomeScreen extends Component {
@@ -12,7 +15,26 @@ class HomeScreen extends Component {
   }
 
   render() {
-    return <Map />;
+    return (
+      <View style={styles.container}>
+
+        <View style={styles.nav} >
+          <View>
+            {/* <Ionicons name="md-menu" size={32} color="white" /> */}
+          </View>
+          <View>
+            <Text style={styles.navText} >Home</Text>
+          </View>
+        </View>
+
+        <View>
+          <Text>HomeScreen</Text>
+        </View>
+        <View style={styles.mapContainer} >
+          <Map />
+        </View>
+      </View>
+    );
   }
 }
 
@@ -27,6 +49,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "flex-start"
+  },
+  mapContainer: {
+    height: 500,
+    width: "100%"
+  },
+  nav: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#273444",
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    padding: 10
+  },
+  navText: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold"
   }
 });
