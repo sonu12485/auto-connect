a543959f891b70a66ff14d29822f85661bfc961e
sonu12485 <sonusaikishan@gmail.com>
2019-02-10 19:05:27 +0530

    Some changes

--- auto-connect/screens/Routes.js
+++ auto-connect/screens/Routes.js
@@ -1,21 +1,96 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Picker } from "react-native";
 
-class Routes extends Component {
+import { fetchUserDetails } from "../actions/userDetails";
+
+import { connect } from "react-redux";
+
+import Map from "../components/Map";
+
+class HomeScreen extends Component {
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      start: "java",
+      end: "js"
+    }
+  }
+
+  componentDidMount() {
+    this.props.fetchUserDetails();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Routes</Text>
+        <View style={styles.inputContainer} >
+
+        <View>
+          <View>
+            <Text>Enter start Location</Text>
+          </View>
+          <View>
+            <Picker
+              mode="dropdown"
+              selectedValue={this.state.start}
+              style={{height: 60, width: 300}}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({start: itemValue})
+              }>
+              <Picker.Item label="Java" value="java" />
+              <Picker.Item label="JavaScript" value="js" />
+            </Picker>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>Enter end Location</Text>
+          </View>
+          <View>
+            <Picker
+              mode="dropdown"
+              selectedValue={this.state.end}
+              style={{height: 60, width: 300}}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({end: itemValue})
+              }>
+              <Picker.Item label="Java" value="java" />
+              <Picker.Item label="JavaScript" value="js" />
+            </Picker>
+          </View>
+        </View>
+        
+
+        </View>
+        <View style={styles.mapContainer} >
+          <Map />
+        </View>
       </View>
     );
   }
 }
-export default Routes;
+
+export default connect(
+  null,
+  {
+    fetchUserDetails
+  }
+)(HomeScreen);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "flex-start"
+  },
+  mapContainer: {
+    height: 500,
+    width: "100%"
+  },
+  inputContainer: {
+    marginTop: 20
   }
 });
