f8e99aee538691399be67cba12c48e172a4595a0
sonu12485 <sonusaikishan@gmail.com>
2019-02-10 23:46:52 +0530

    Added locations and routes api

---
+++ backend/db/data/locations.js
@@ -0,0 +1,70 @@
+const locations = {
+    "locations": [
+        {
+            "name": "Silk Board Junction",
+            "lat": 12.917954,
+            "long": 77.623323,
+            "id": 1
+        },
+        {
+            "name": "Marathahalli Bridge",
+            "lat": 12.956817,
+            "long": 77.701244,
+            "id": 2
+        },
+        {
+            "name": "Hal main gate",
+            "lat": 12.959123,
+            "long": 77.663553,
+            "id": 3
+        },
+        {
+            "name": "Kundalahalli gate",
+            "lat": 12.956159,
+            "long": 77.713699,
+            "id": 4
+        },
+        {
+            "name": "White field post office",
+            "lat": 12.9677314,
+            "long": 77.7472623,
+            "id": 5
+        },{
+            "name": "Hope farm",
+            "lat": 12.9837667,
+            "long": 77.7503005,
+            "id": 6
+        },
+        {
+            "name": "Brigrade road",
+            "lat": 12.971967,
+            "long": 77.607004,
+            "id": 7
+        },
+        {
+            "name": "Koramangala",
+            "lat": 12.9348958,
+            "long": 77.6098841,
+            "id": 8
+        },
+        {
+            "name": "Domlur",
+            "lat": 12.9599114,
+            "long": 77.6393502,
+            "id": 9
+        },{
+            "name": "Electronic city",
+            "lat": 12.843423,
+            "long": 77.676673,
+            "id": 10
+        },
+        {
+            "name": "Indiranagar",
+            "lat": 12.973739,
+            "long": 77.641445,
+            "id": 11
+        }
+    ]
+};
+
+module.exports = locations;
