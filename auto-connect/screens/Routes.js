e33083bcdb9b32af7aff8a900f41ab955ba86715
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2019-02-10 20:18:14 +0530

    Dynamically render location of user

--- auto-connect/screens/Routes.js
+++ auto-connect/screens/Routes.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Picker } from "react-native";
-
+import { Platform, View, Text, StyleSheet, Picker } from "react-native";
+import { Constants, Location, Permissions } from "expo";
 import { fetchUserDetails } from "../actions/userDetails";
 
 import { connect } from "react-redux";
@@ -8,65 +8,105 @@ import { connect } from "react-redux";
 import Map from "../components/Map";
 
 class HomeScreen extends Component {
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
 
     this.state = {
+      location: null,
+      locationErrorMessage: null,
       start: "java",
       end: "js"
-    }
+    };
   }
 
   componentDidMount() {
     this.props.fetchUserDetails();
+
+    if (Platform.OS === "android" && !Constants.isDevice) {
+      this.setState({
+        locationErrorMessage:
+          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
+      });
+    } else {
+      this._getLocationAsync();
+    }
   }
 
+  _getLocationAsync = async () => {
+    try {
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status !== "granted") {
+        this.setState({
+          locationErrorMessage: "Location access denied"
+        });
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      this.setState({ location });
+    } catch (e) {
+      console.log(e);
+    }
+  };
   render() {
+    let locationText = "Waiting..";
+    if (this.state.locationErrorMessage) {
+      locationText = this.state.locationErrorMessage;
+    } else if (this.state.location) {
+      locationText = JSON.stringify(this.state.location);
+    }
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer} >
+        <View style={styles.inputContainer}>
+          <View>
+            <View>
+              <Text>Enter start Location</Text>
+            </View>
+            <View>
+              <Picker
+                mode="dropdown"
+                selectedValue={this.state.start}
+                style={{ height: 60, width: 300 }}
+                onValueChange={(itemValue, itemIndex) =>
+                  this.setState({ start: itemValue })
+                }
+              >
+                <Picker.Item label="Java" value="java" />
+                <Picker.Item label="JavaScript" value="js" />
+              </Picker>
+            </View>
+          </View>
 
+          <View>
+            <View>
+              <Text>Enter end Location</Text>
+            </View>
+            <View>
+              <Picker
+                mode="dropdown"
+                selectedValue={this.state.end}
+                style={{ height: 60, width: 300 }}
+                onValueChange={(itemValue, itemIndex) =>
+                  this.setState({ end: itemValue })
+                }
+              >
+                <Picker.Item label="Java" value="java" />
+                <Picker.Item label="JavaScript" value="js" />
+              </Picker>
+            </View>
+          </View>
+        </View>
         <View>
-          <View>
-            <Text>Enter start Location</Text>
-          </View>
-          <View>
-            <Picker
-              mode="dropdown"
-              selectedValue={this.state.start}
-              style={{height: 60, width: 300}}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({start: itemValue})
-              }>
-              <Picker.Item label="Java" value="java" />
-              <Picker.Item label="JavaScript" value="js" />
-            </Picker>
-          </View>
+          <Text>{locationText}</Text>
         </View>
-
-        <View>
-          <View>
-            <Text>Enter end Location</Text>
-          </View>
-          <View>
-            <Picker
-              mode="dropdown"
-              selectedValue={this.state.end}
-              style={{height: 60, width: 300}}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({end: itemValue})
-              }>
-              <Picker.Item label="Java" value="java" />
-              <Picker.Item label="JavaScript" value="js" />
-            </Picker>
-          </View>
-        </View>
-        
-
-        </View>
-        <View style={styles.mapContainer} >
-          <Map />
+        <View style={styles.mapContainer}>
+          <Map
+            lat={
+              this.state.location && Number(this.state.location.coords.latitude)
+            }
+            long={
+              this.state.location &&
+              Number(this.state.location.coords.longitude)
+            }
+          />
         </View>
       </View>
     );
