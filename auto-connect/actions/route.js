76808d4af41ac2800cf24d1cecc8652db0de9241
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2019-02-11 20:41:43 +0530

    Some changes

--- auto-connect/actions/route.js
+++ auto-connect/actions/route.js
@@ -5,35 +5,31 @@ const URL = process.env["BACKEND_URI"];
 import { AsyncStorage } from "react-native";
 
 export const fetchRoute = (from, to) => {
+  return dispatch => {
+    AsyncStorage.getItem("token").then(token => {
+      axios({
+        url: `${URL}route`,
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+        data: {
+          from,
+          to
+        }
+      })
+        .then(res => {
+          console.log(res.data);
 
-    return dispatch => {
-
-        AsyncStorage.getItem("token")
-            .then( token => {
-                axios({
-                    url: `${URL}route`,
-                    method:'post',
-                    headers:{
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`
-                    },
-                    data:{
-                        from,
-                        to
-                    }
-                })
-                .then( res => {
-
-                    console.log(res.data);
-
-                    dispatch({
-                        type: "FETCH_ROUTE",
-                        payload: res.data
-                    })
-                })
-                .catch( err => {
-                    console.log("error", err);
-                });
-            })
-    }
-} 
+          dispatch({
+            type: "FETCH_ROUTE",
+            payload: res.data
+          });
+        })
+        .catch(err => {
+          console.log("error", err);
+        });
+    });
+  };
+};
