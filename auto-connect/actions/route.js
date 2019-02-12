cc592c81a73743dab4a01be8567193478aeba087
sonu12485 <sonusaikishan@gmail.com>
2019-02-12 20:51:25 +0530

    Added Polyline for auto route

--- auto-connect/actions/route.js
+++ auto-connect/actions/route.js
@@ -4,6 +4,11 @@ const URL = process.env["BACKEND_URI"];
 
 import { AsyncStorage } from "react-native";
 
+import Polyline from "@mapbox/polyline";
+
+const API_KEY = "AIzaSyCvEFK1EMeKuwvshOn8NAS214I2WzrEPgc";
+
+
 export const fetchRoute = (from, to) => {
 
     return dispatch => {
@@ -24,12 +29,32 @@ export const fetchRoute = (from, to) => {
                 })
                 .then( res => {
 
-                    console.log(res.data);
+                    const route = res.data;
 
-                    dispatch({
-                        type: "FETCH_ROUTE",
-                        payload: res.data
-                    })
+                    axios.get(
+                    `https://maps.googleapis.com/maps/api/directions/json?origin=${route[0].lat},${route[0].long}&destination=${route[1].lat},${route[1].long}&mode=walking&key=${API_KEY}`
+                    )
+                    .then(result => result.data)
+                    .then(result => {
+                        let array = Polyline.decode(result.routes[0].overview_polyline.points);
+                        let coordinates = array.map((point) => {
+                                return  {
+                                    latitude :point[0],
+                                    longitude :point[1]
+                                }
+                            })
+
+                        dispatch({
+                            type: "FETCH_ROUTE",
+                            payload: {
+                                route,
+                                polyline: coordinates
+                            }
+                        });
+
+                    }).catch(er => {
+                        console.log(er.message);
+                    });
                 })
                 .catch( err => {
                     console.log("error", err);
--- auto-connect/components/Map.js
+++ auto-connect/components/Map.js
@@ -1,16 +1,39 @@
 import React from "react";
 import { MapView } from "expo";
 
+import { connect } from "react-redux";
+
 // PROPS: lat,long
 
 class Map extends React.Component {
+
+  renderPolyline = () => {
+
+    if(this.props.route.polyline !== null)
+    {
+      return (
+        <MapView.Polyline
+          coordinates={this.props.route.polyline}
+          strokeWidth={4}
+          strokeColor="rgba(255,140,0,0.8)"
+        />
+      );
+    }
+    else
+    {
+      return null;  
+    }
+  }
+
   render() {
     return (
       <MapView
         style={{ flex: 1 }}
         initialRegion={{
-          latitude: this.props.lat,
-          longitude: this.props.long,
+          // latitude: this.props.lat,
+          // longitude: this.props.long,
+          latitude: 12.973739,
+          longitude: 77.641445,
           latitudeDelta: 0.01,
           longitudeDelta: 0.01
         }}
@@ -22,9 +45,16 @@ class Map extends React.Component {
           }}
           title="Your Location"
         />
+        {this.renderPolyline()}
       </MapView>
     );
   }
 }
 
-export default Map;
+const mapStateToProps = state => {
+  return {
+    route: state.route
+  };
+};
+
+export default connect(mapStateToProps)(Map);
