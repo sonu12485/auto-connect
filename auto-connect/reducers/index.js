6d63a719297c45e1679b3dd86e6799ce3b3868b8
sonu12485 <sonusaikishan@gmail.com>
2019-02-16 15:20:27 +0530

    Added QR code reader

--- auto-connect/reducers/index.js
+++ auto-connect/reducers/index.js
@@ -4,12 +4,14 @@ import userReducer from './userReducer';
 import placesReducer from './placesReducer';
 import routeReducer from './routeReducer';
 import pickupReducer from './pickupReducer';
+import qrCodeReducer from "./qrCodeReducer";
 
 const rootReducer = combineReducers({
     user: userReducer,
     places: placesReducer,
     route: routeReducer,
-    pickUpRoute: pickupReducer
+    pickUpRoute: pickupReducer,
+    qrCode: qrCodeReducer
 });
 
 export default rootReducer;
--- auto-connect/screens/Pay.js
+++ auto-connect/screens/Pay.js
@@ -1,18 +1,58 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { BarCodeScanner, Permissions } from 'expo';
+
+import { storeQRCodeData } from "../actions/qrCode";
+
+import { connect } from "react-redux";
 
 class Pay extends Component {
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      hasCameraPermission: null
+    }
+  }
+
+  async componentDidMount() 
+  {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  handleBarCodeScanned = ({ type, data }) => {
+    console.log(data);
+    this.props.storeQRCodeData(data);
+  }
 
   render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
     return (
       <View style={styles.container}>
-        <Text>Pay</Text>
+        <BarCodeScanner
+          onBarCodeScanned={this.handleBarCodeScanned}
+          style={{
+            height: Dimensions.get('window').height/2,
+            width: Dimensions.get('window').width/2,
+          }}
+        />
       </View>
     );
   }
 }
 
-export default Pay;
+export default connect(null, {
+  storeQRCodeData
+})(Pay);
 
 const styles = StyleSheet.create({
   container: {
