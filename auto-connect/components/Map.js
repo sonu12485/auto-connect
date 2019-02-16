8fa41579da4e09ec461d65d965ef89b58c481501
sonu12485 <sonusaikishan@gmail.com>
2019-02-16 11:26:19 +0530

    PickUp Completed

--- auto-connect/components/Map.js
+++ auto-connect/components/Map.js
@@ -47,20 +47,20 @@ class Map extends React.Component {
       <MapView
         style={{ flex: 1 }}
         initialRegion={{
-          // latitude: this.props.lat,
-          // longitude: this.props.long,
-          latitude: 12.973739,
-          longitude: 77.641445,
+          latitude: this.props.lat,
+          longitude: this.props.long,
+          // latitude: 12.973739,
+          // longitude: 77.641445,
           latitudeDelta: 0.01,
           longitudeDelta: 0.01
         }}
       >
         <MapView.Marker
           coordinate={{
-            // latitude: this.props.lat ? this.props.lat : 0,
-            // longitude: this.props.long ? this.props.long : 0
-            latitude: 12.973739,
-            longitude: 77.641445
+            latitude: this.props.lat ? this.props.lat : 0,
+            longitude: this.props.long ? this.props.long : 0
+            // latitude: 12.973739,
+            // longitude: 77.641445
           }}
           title="Your Location"
         />
